d6d6c9c282d6e04811d50279f97f76262569532d
Larissa Borges <larissaborges@Mac.home>
2026-08-31 11:06:21 -0300

    feat: adiciona telas de login e autocadastro

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Autocadastro } from './autocadastro/autocadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'cadastro', component: Autocadastro },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
